c4ce81f9abc35189c6362cc2d701673563afc280
ParkJaeHyun <hyunihyuni04@gmail.com>
2023-08-26 16:56:20 +0900

    add annotation

--- reportcheck/views.py
+++ reportcheck/views.py
@@ -8,14 +8,19 @@ from django.http import JsonResponse
 
 @api_view(["GET"])
 def myreport(request):
+    # 내 신고 내역 조회 
     if Member.objects.filter(MemberID=request.data["MemberID"]).exists():
+        # request 데이터의 ID와 부합하는 Member 데이터가 있으면
         user = Member.objects.get(MemberID=request.data["MemberID"])
-    else:
+    else: 
         return JsonResponse({"message": "등록되지 않은 ID입니다."})
     
     if ReportList.objects.filter(AfterUniqueNumber=user.UniqueNumber).exists():
+        # request 된 user가 신고한 내역이 있을 경우
+        # response 될 데이터셋을 위한 리스트 생성
         report_list = ReportList.objects.filter(AfterUniqueNumber=user.UniqueNumber).order_by('-AfterDate')
         Car_num = [num.CarNum for num in report_list]
+        # 위도 경도를 response 하기 위한 car_list 
         car_list = CarList.objects.filter(MemberID=request.data["MemberID"], CarNum__in=Car_num).order_by('-Date')
     else:
         return JsonResponse({"message": "신고 내역이 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
@@ -35,9 +40,13 @@ def myreport(request):
 
 @api_view(["GET"])
 def mycar(request):
+    # 내 차 신고된 내역 조회
     if ReportList.objects.filter(CarNum=request.data["CarNum"]).exists():
+        # 신고 내역에 request 된 차 번호와 부합하는 데이터가 있으면
+        # response 될 데이터셋을 위한 리스트 생성
         report_list = ReportList.objects.filter(CarNum=request.data["CarNum"]).order_by('-AfterDate')
         Car_num = [num.CarNum for num in report_list]
+        # 위도 경도를 response 하기 위한 car_list
         car_list = CarList.objects.filter(CarNum__in=Car_num).order_by('-Date')
     else:
         return JsonResponse({"message": "신고 내역에 없는 차량 번호 입니다."})
--- send/views.py
+++ send/views.py
@@ -15,11 +15,10 @@ def Data(request):
     #UnboundLocalError: local variable 'recent_5min' referenced before assignment 에러발생
     recent_5min=None
     if request.method == 'POST':
-        if CarList.objects.filter(CarNum=request.data["CarNum"], ReportStatus='F').exists():
-            #다음 코드는 똑같은 값이 여러개 있을 때 에러가 남
-            #recent_5min=CarList.objects.get(CarNum=request.data["CarNum"])
+        if CarList.objects.filter(CarNum=request.data["CarNum"], ReportStatus='F').exists(): # request된 차 번호와 같은 번호의 미신고된 데이터가 있을 경우
             recent_5min = CarList.objects.filter(CarNum=request.data["CarNum"], ReportStatus='F').order_by('-Date').first()
             if recent_5min and datetime.strptime(request.data["Date"], '%Y-%m-%d %H:%M:%S').replace(tzinfo=pytz.UTC)-recent_5min.Date >= timedelta(minutes=5):
+                # 해당 미신고된 데이터가 request 시점으로부터 5분 이상의 데이터면 
                 
                 # 여기에 위도, 경도 비교 기능 작성
 
@@ -35,7 +34,7 @@ def Data(request):
 
                 # 거리 계산
                 #print(haversine(current, new, unit = 'm'))
-                if haversine(current, new, unit = 'm')<=1.75:
+                if haversine(current, new, unit = 'm')<=1.75: # 오차범위 내의 경우
                     recent_5min.ReportStatus = 'T' 
                     recent_5min.save()
                     data = {
@@ -51,15 +50,15 @@ def Data(request):
                     else:
                         print(serializer.errors)
                         return JsonResponse({'message': 'ReportListSerializer error', 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-            else:
+            else: # 해당 미신고된 데이터가 request 시점으로부터 5분 이상의 데이터가 아니면
                 pass
-        else:
+        else: # request된 차 번호와 같은 번호의 미신고된 데이터가 없을 경우
             pass
         dataset = {}
         for i in request.data:
             dataset[i] = request.data[i]
-        if recent_5min!=None and recent_5min.ReportStatus == 'T':
-            dataset['ReportStatus'] = 'T'
+        if recent_5min!=None and recent_5min.ReportStatus == 'T': # 앞서 정의된 recent_5min가 존재하고 ReportStatus가 T인 경우
+            dataset['ReportStatus'] = 'T' 
         serializer = CarListSerializers(data=dataset)
         if serializer.is_valid():
             serializer.save()
